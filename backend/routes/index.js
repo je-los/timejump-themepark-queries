b9fef2a834df813f4c5c1bed2087b2fac00b988f
losoyajesus2 <losoyajesus2@gmail.com>
2025-10-23 11:48:23 -0500

    added basic working code to backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,1 +1,19 @@
-//helllo
+const url = require('url');
+
+const router = (req, res) => {
+    const parsedURL = url.parse(req.url, true);
+    const path = parsedURL.pathname;
+
+    console.log(`Request received for: ${req.method} ${path}}`);
+
+    if (req.method === 'GET' && path === '/api/test') {
+        res.writeHead(200, { 'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: 'Success! router is working'}));
+    }
+    else{
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({message: 'Route not found'}));
+    }
+};
+
+module.exports = router;
