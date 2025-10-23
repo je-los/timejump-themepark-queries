09faa590d88139c6e3c50df3cd56c687eb9a98bb
losoyajesus2 <losoyajesus2@gmail.com>
2025-10-23 11:48:23 -0500

    added basic working code to backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const mainRouter = require('./routes/index.js');
+
+const server = http.createServer((req,res) => {
+    mainRouter(req, res);
+
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`); //BACKTICK ` ` NOT ' ', BACKTICK IS NEXT TO 1 ON KEYBOARD
+});
