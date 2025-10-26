f4714f00667b2093bfab2aff7e7307829062d07e
losoyajesus2 <losoyajesus2@gmail.com>
2025-10-25 20:07:09 -0500

    the base api for attractions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,18 +1,40 @@
 const url = require('url');
-
+const attractionController = require('../controllers/attractionController');
 const router = (req, res) => {
     const parsedURL = url.parse(req.url, true);
     const path = parsedURL.pathname;
 
     console.log(`Request received for: ${req.method} ${path}}`);
 
-    if (req.method === 'GET' && path === '/api/test') {
-        res.writeHead(200, { 'Content-Type': 'application/json'});
-        res.end(JSON.stringify({message: 'Success! router is working'}));
+    const attractionIdMatch = path.match(/^\/api\/attractions\/([0-9]+)$/);
+
+    if (req.method === 'GET' && path === '/api/attractions') {
+    attractionController.getAllAttractions(req, res);
+  
+    } 
+    else if (req.method === 'GET' && attractionIdMatch) {
+    const id = attractionIdMatch[1];
+    attractionController.getAttractionById(req, res, id);
     }
-    else{
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({message: 'Route not found'}));
+    else if (req.method === 'POST' && path === '/api/attractions') {
+        attractionController.createAttraction(req, res);
+
+        // --- (You would add other resources here) ---
+        // } else if (req.method === 'GET' && path === '/api/employees') {
+        //   employeeController.getAllEmployees(req, res);
+  
+    }
+    else if (req.method === 'PUT' && attractionIdMatch) {
+        const id = attractionIdMatch[1];
+        attractionController.updateAttraction(req, res, id);
+    }
+    else if (req.method === 'DELETE' && attractionIdMatch) {
+        const id = attractionIdMatch[1];
+        attractionController.deleteAttraction(req, res, id);
+    }
+    else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
     }
 };
 
